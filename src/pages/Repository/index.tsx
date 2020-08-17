501e67f07e818998c1a6b3ff5f94b5edc5ecc978
Luís Gustavo <luis.gustavo_18@outlook.com>
2020-08-17 07:45:34 -0300

    Etilizando a página de detalhes do Repositório

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/img/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
     repository: string;
@@ -10,7 +15,53 @@ const Repository: React.FC = () => {
     const { params } = useRouteMatch<RepositoryParams>();
 
     return (
-    <h1>Repository {params.repository}</h1>
+        <>
+            <Header>
+                <img src={logoImg} alt="Github Explorer" />
+                <Link to="/">
+                    <FiChevronLeft size={16} />
+                    Voltar
+                </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img 
+                        src="https://avatars1.githubusercontent.com/u/26330809?s=460&u=73f8d6d327f025dd3faf3afa01cc4c795a117e04&v=4"
+                        alt="Luis Gustavo"
+                    />
+                    <div>
+                        <strong>louisGustavo/projetoCSSGrid</strong>
+                        <p>Descrição do repositório</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1808</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>48</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>67</strong>
+                        <span>Issues abertas</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+                <Link to="askdhasdk" >
+                    <div>
+                        <strong>asdasdasd</strong>
+                        <p>asdasdasdas</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </Link>
+            </Issues>
+        </>
     )
 }
 
